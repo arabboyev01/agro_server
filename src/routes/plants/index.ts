27b84f6323a003ee2a5688c25686da5b7d6330f0
Abbosbek <abbosarabboyev9@gmail.com>
2024-03-09 14:16:46 +0500

    wserds

--- src/routes/plants/index.ts
+++ src/routes/plants/index.ts
@@ -9,6 +9,7 @@ const plantsProducts = Router()
 
 class PlantProducts {
     async createPlantsCategory(req: AuthRequest, res: Response) {
+        console.log(req.body)
         try {
             if (!req.isAdmin) {
                 return res.status(403).json({ success: false, message: "Unathorized" })
