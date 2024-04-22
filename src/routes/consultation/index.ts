db30b682613e0f55594f966fe451952294a29903
abbosbek <abbosarabboyev9@gmail.com>
2024-04-22 09:27:50 +0500

    consultant-page

--- src/routes/consultation/index.ts
+++ src/routes/consultation/index.ts
@@ -8,22 +8,23 @@ const consultationRouter = Router()
 
 class ConsultationController {
     async creaateConsultation(req: AuthRequest, res: Response) {
-        // try {
-            const { fullName, dagree, phone_number, image } = req.body
+        try {
+            const { fullName, dagree, phone_number } = req.body
+            const imageUrl: string = req.imageUrl as string
 
             const newConsultation = await prisma.consultation.create({
                 data: {
                     fullName,
-                    image,
+                    image: imageUrl,
                     dagree,
                     phone_number,
                 }
             })
             console.log(newConsultation)
             return res.json({ success: true, data: newConsultation })
-        // } catch (error) {
-        //     return res.status(500).json({ success: false, error: "Unable to create consultation" })
-        // }
+        } catch (error) {
+            return res.status(500).json({ success: false, error: "Unable to create consultation" })
+        }
     }
 
     async getConsultation(req: AuthRequest, res: Response) {
@@ -54,12 +55,13 @@ class ConsultationController {
         try {
             const id = parseInt(req.params.id)
             const { fullName, dagree, phone_number, image } = req.body
+            const imageUrl: string = req.imageUrl as string
 
             const updatedPlantsCategory = await prisma.consultation.update({
                 where: { id },
                 data: {
                     fullName,
-                    image,
+                    image: imageUrl,
                     dagree,
                     phone_number,
                 }
@@ -85,7 +87,7 @@ class ConsultationController {
 
 const consultationController = new ConsultationController()
 
-consultationRouter.post("/", auth, consultationController.creaateConsultation)
+consultationRouter.post("/", auth, storageDisk, consultationController.creaateConsultation)
 consultationRouter.get("/", consultationController.getConsultation)
 consultationRouter.get("/:id", consultationController.getConsultationById)
 consultationRouter.put("/:id", auth, storageDisk, consultationController.updatePlantsCategory)
