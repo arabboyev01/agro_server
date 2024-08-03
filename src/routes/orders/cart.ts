d54b0cfa77480aa9150e4f013743b0037d53eb87
arabboyev01 <abbosarabboyev9@gmail.com>
2024-08-03 17:40:35 +0500

    some changes

--- src/routes/orders/cart.ts
+++ src/routes/orders/cart.ts
@@ -40,34 +40,15 @@ const gupdateCArtorders = async (req: AuthRequest, res: Response) => {
     }
 }
 
-const getOrders = async (req: Request, res: Response) => {
-  try {
-    const data = prisma.cart.create({
-      data: { ...req.body },
-    });
-
-    if (!data) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Something went wrong " });
-    }
-
-    return res.status(201).json({ success: true, data });
-  } catch (err: unknown) {
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: "Interna server error" + (err as Error).message,
-      });
-  }
-};
 
 const getArtorders = async (req: AuthRequest, res: Response) => {
   try {
 
     const data = prisma.cart.findMany({
       where: { userId: Number(req?.user?.id) },
+      include: {
+        prodcuts: true
+      }
     });
 
     if (!data) {
