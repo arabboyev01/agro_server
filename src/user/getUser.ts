26635ef01b9e0447b8dd2fbdb0e79d72996edc16
arabboyev01 <abbosarabboyev9@gmail.com>
2024-08-07 16:45:35 +0500

    some changes

--- src/user/getUser.ts
+++ src/user/getUser.ts
@@ -1,24 +1,38 @@
-import { prisma } from "../prisma/client";
-import { Router, Response } from "express";
-import { AuthRequest } from "../types/global";
-import { error, success } from "../global/error";
-import { auth } from "../auth";
+import { prisma } from "../prisma/client"
+import { Router, Response } from "express"
+import { AuthRequest } from "../types/global"
+import { error, success } from "../global/error"
+import { auth } from "../auth"
 
-const getUser = Router();
+const getUser = Router()
 
-getUser.get("/", auth, async (req: AuthRequest, res: Response) => {
+const users = async (req: AuthRequest, res: Response) => {
+  try {
+    const user = await prisma.user.findMany();
+
+    return res.status(200).json({ ...success, message: "user data", user });
+  } catch (err) {
+    return res.status(501).json({ ...error });
+  }
+};
+
+const user = async (req: AuthRequest, res: Response) => {
   try {
     const user = await prisma.user.findUnique({
-      where: { id: Number(req.user.id) }
-    })
+      where: { id: Number(req.user.id) },
+    });
 
     if (!user) {
-      return res.status(403).json({ ...error })
+      return res.status(403).json({ ...error });
     }
 
-    return res.status(200).json({ ...success, message: "user data", user })
+    return res.status(200).json({ ...success, message: "user data", user });
   } catch (err) {
     return res.status(501).json({ ...error })
   }
-})
+}
+
+getUser.get("/all", auth, users)
+getUser.get("/", auth, user)
+
 export default getUser
