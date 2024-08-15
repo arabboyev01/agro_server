e83020b7fc71b7b30dde82c7d0a340151bec1f25
arabboyev01 <abbosarabboyev9@gmail.com>
2024-08-15 14:06:41 +0500

    ss

--- src/server.ts
+++ src/server.ts
@@ -20,15 +20,12 @@ import distrcitsByRegionId from './controllers/district/byRegionId/byRegionId'
 import varityDetails from './controllers/varities/varityController'
 import TypesByCategories from './controllers/plants/typeByCategories'
 import questionRouter from './controllers/questionRoute/questionRoute'
-import { corsOptions } from './utils/cors/cors'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.use(cors())
 app.use('/image', express.static('image'))
-app.use(express.json({ limit: '50mb' }))
 
 const PORT = process.env.PORT || 3500
 
